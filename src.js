81368adcec12b6c578a48eace9c04979faf45aef
Jody Salt <jody.salt@stfc.ac.uk>
2016-06-14 16:55:15 +0100

    can now resize canvas

--- src.js
+++ src.js
@@ -7,6 +7,7 @@
 	(function(){
 
 		function NoVnc(canvas, host, port, password, path){
+
 			var rfb = new RFB({
 				'target': canvas,
 				'encrypt': WebUtil.getQueryVar('encrypt', (window.location.protocol === "https:")),
@@ -14,12 +15,54 @@
 				'true_color': WebUtil.getQueryVar('true_color', true),
 				'local_cursor': WebUtil.getQueryVar('cursor', true),
 				'shared': WebUtil.getQueryVar('shared', true),
-				'view_only': WebUtil.getQueryVar('view_only', false)
+				'view_only': WebUtil.getQueryVar('view_only', false),
+				'onUpdateState': onUpdateState
 			});
+
+			var originalWidth;
+			var originalHeight;
+
+			var isLoaded = 0;
+			var afterLoadedList = [];
+			function afterLoaded(fn){
+				if(!isLoaded){
+					afterLoadedList.push(fn);
+				} else {
+					fn();
+				}
+			}
+
+
+			function onUpdateState(rfb, state, oldstate, statusMsg){
+				console.log('state', state);
+				if(state == 'normal'){
+					isLoaded = true;
+					originalWidth = $(canvas).width();
+					originalHeight = $(canvas).height();
+
+					while(afterLoadedList.length > 0){
+						afterLoadedList.shift()();
+					}
+				}
+			}
+
 
 			rfb.connect(host, port, password, path);
 
+			this.resize = function(width, height){
+				afterLoaded(function(){
+					var widthScale = width / originalWidth;
+					var heightScale = height / originalHeight;
 
+					var scale = widthScale < heightScale ? widthScale : heightScale;
+
+					var newWidth = originalWidth * scale;
+					var newHeight = originalHeight * scale;
+					
+					$(canvas).width(newWidth).height(newHeight);
+					rfb.get_mouse().set_scale(scale);
+				});
+			};
 
 		}
 
