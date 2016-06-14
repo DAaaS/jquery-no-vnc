561ea0723889805d60068a1bfdcd17a86a0e1983
Jody Salt <jody.salt@stfc.ac.uk>
2016-06-14 18:08:34 +0100

    refactor

--- src.js
+++ src.js
@@ -14,12 +14,11 @@
 
 			var rfb = new RFB({
 				'target': canvas,
-				'encrypt': WebUtil.getQueryVar('encrypt', (window.location.protocol === "https:")),
-				'repeaterID': WebUtil.getQueryVar('repeaterID', ''),
-				'true_color': WebUtil.getQueryVar('true_color', true),
-				'local_cursor': WebUtil.getQueryVar('cursor', true),
-				'shared': WebUtil.getQueryVar('shared', true),
-				'view_only': WebUtil.getQueryVar('view_only', false),
+				'encrypt': window.location.protocol === "https:",
+				'true_color': true,
+				'local_cursor': true,
+				'shared': true,
+				'view_only': false,
 				'onUpdateState': onUpdateState,
 				'onFBUComplete': onFBUComplete
 			});
